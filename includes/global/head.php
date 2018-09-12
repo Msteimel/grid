129d5dbbc56c1a89d781b87675fb77af140de1ea
Mitch Steimel - home <mitch.steimel@gmail.com>
2018-09-12 06:07:16 -0500

    Node and NPM

---
+++ includes/global/head.php
@@ -0,0 +1,13 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <!-- End Font Awesome -->
+  <!-- Fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Work+Sans" rel="stylesheet">
+
+
+  <link rel="stylesheet" href="/css/global.css">
+</head>
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1>hello</h1>
+
+<?php include 'includes/global/head.php';?>
+
+<div class="grid-12">
+	<div class="col-1">
+		1
+	</div>
+	<div class="col-11">
+		11
+	</div>
+</div>
