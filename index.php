6d052676a66e0a177f9be669afa2930189f17994
Mitch Steimel - home <mitch.steimel@gmail.com>
2018-09-13 20:23:10 -0500

    Node and NPM + Starting grid + breakpoint mixins

--- index.php
+++ index.php
@@ -1,11 +1,74 @@
 
 <?php include 'includes/global/head.php';?>
 
-<div class="grid-12">
+<div class="grid-12 example-grid">
 	<div class="col-1">
-		1
+		<div class="example-col">1</div>
 	</div>
 	<div class="col-11">
-		11
+		<div class="example-col">11</div>
+	</div>
+	<div class="col-2">
+		<div class="example-col">2</div>
+	</div>
+	<div class="col-10">
+		<div class="example-col">10</div>
+	</div>
+	<div class="col-3">
+		<div class="example-col">3</div>
+	</div>
+	<div class="col-9">
+		<div class="example-col">9</div>
+	</div>
+	<div class="col-4">
+		<div class="example-col">4</div>
+	</div>
+	<div class="col-8">
+		<div class="example-col">8</div>
+	</div>
+	<div class="col-5">
+		<div class="example-col">5</div>
+	</div>
+	<div class="col-7">
+		<div class="example-col">7</div>
+	</div>
+	<div class="col-6">
+		<div class="example-col">6</div>
+	</div>
+	<div class="col-6">
+		<div class="example-col">6</div>
+	</div>
+	<div class="col-7">
+		<div class="example-col">7</div>
+	</div>
+	<div class="col-5">
+		<div class="example-col">5</div>
+	</div>
+	<div class="col-8">
+		<div class="example-col">8</div>
+	</div>
+	<div class="col-4">
+		<div class="example-col">4</div>
+	</div>
+	<div class="col-9">
+		<div class="example-col">9</div>
+	</div>
+	<div class="col-3">
+		<div class="example-col">3</div>
+	</div>
+	<div class="col-10">
+		<div class="example-col">10</div>
+	</div>
+	<div class="col-2">
+		<div class="example-col">2</div>
+	</div>
+	<div class="col-11">
+		<div class="example-col">11</div>
+	</div>
+	<div class="col-1">
+		<div class="example-col">1</div>
+	</div>
+	<div class="col-12">
+		<div class="example-col">12</div>
 	</div>
 </div>
