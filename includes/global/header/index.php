b7e4708e3e6b855a771770dac412929df4812305
Mitchell Steimel <msteimel@trilixgroup.com>
2018-10-03 16:31:56 -0500

    moving include files + starting on main nav.

---
+++ includes/global/header/index.php
@@ -0,0 +1,95 @@
+
+<nav class="header-nav">
+	<ul class="navigation-main">
+		<li class="nav-1">
+			<a href="#">Nav 1</a>
+			<nav class="nav-menu">
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+					<li><a href="#">Link 6</a></li>
+				</ul>
+			</nav>
+		</li>
+		<li class="nav-2">
+			<a href="#">Nav 2</a>
+			<div class="nav-menu">
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+					<li><a href="#">Link 6</a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="nav-3">
+			<a href="#">Nav 3</a>
+			<div class="nav-menu">
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+					<li><a href="#">Link 6</a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="nav-4">
+			<a href="#">Nav 4</a>
+			<div class="nav-menu">
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+					<li><a href="#">Link 6</a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="nav-5">
+			<a href="#">Nav 5</a>
+			<div class="nav-menu">
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+					<li><a href="#">Link 6</a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="nav-6">
+			<a href="#">Nav 6</a>
+			<div class="nav-menu">
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+				<ul class="nav-menu--col">
+					<li><a href="#">Link 4</a></li>
+					<li><a href="#">Link 5</a></li>
+					<li><a href="#">Link 6</a></li>
+				</ul>
+			</div>
+		</li>
+	</ul>
+</nav>
