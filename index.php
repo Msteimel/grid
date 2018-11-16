ca5265a8f8b8966bdb8f9aeede5de5bcd9209cd3
Mitchell Steimel <msteimel@trilixgroup.com>
2018-11-16 15:40:57 -0600

    building out from sketch.

--- index.php
+++ index.php
@@ -6,257 +6,80 @@
 
 
 <body>
-	<div class="grid-12 example-grid container">
-	<?php include 'includes/global/header/index.php';?>
-		<div class="col-12">
-			<h1>Header 1</h1>
-			<h2>Header 2</h2>
-			<h3>Header 3</h3>
-			<h4>Header 4</h4>
-			<h5>Header 5</h5>
-			<h6>Header 6</h6>
-			<p>Elit in do excepteur et commodo pariatur et id cillum mollit nulla voluptate commodo amet sed dolore occaecat ut consequat nulla cupidatat labore laborum eiusmod ad qui dolore.</p>
-			<blockquote>
-				<p>Aliquip adipisicing velit mollit sit incididunt cupidatat irure commodo ea consectetur occaecat consequat.</p> 
-				<cite>Bob</cite>
-			</blockquote>
+
+	<section class="hero">
+		<div class="grid">
+			<?php include 'includes/global/header/index.php';?>
+			<div class="col-12">
+				<div class="hero-content">
+					<h1 class="hero--headline">Another Dumb Framework</h1>
+					<p>I built this to challenge myself and learn more about frameworks and element design.</p>
+				</div>
+			</div>
 		</div>
+		<div class="hero--overlay"></div>	
+	</section>
+
+	<section class="container grid">
+		<div class="col-12">
+			<!-- <div class="col-12"> -->
+				<h1>Type Style</h1>
+				<p>The text is fully responsive by using ems on a per element basis or using an include that allows you set the min and max font size. </p>
+			<!-- </div> -->
+			<div class="block">
+				<div class="col-6">
+					<div class="block-title">Headings</div>
+					<h1>Heading 1</h1>
+					<h2>Heading 2</h2>
+					<h3>Heading 3</h3>
+					<h4>Heading 4</h4>
+					<h5>Heading 5</h5>
+					<h6>Heading 6</h6>
+				</div>
+				<div class="col-6">
+					<div class="block-title">Pararaph Tags</div>
+					<p class="lead">This is a lead paragraph</p>
+					<p><strong>This is a strong paragraph</strong></p>
+					<p>This is a normal paragraph</p>
+					<p><em>This is a em/italic paragraph</em></p>
+					<a href="#">This is an anchor tag</a>
+					<div class="block-title">Code</div>
+					<div class="codeblock">
+						<span>code-block {</span>
+							<span>border: 1px solid #333;</span>
+							<span>padding: 0.5em;</span>
+							<span>text-align: center;</span>
+						<span>}</span>
+					</div>
+					<code>.code { padding: 0.5em; }</code>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="col-4">
+					<ul>
+						<li>Dolore non nisi quis sit.</li>
+						<li>Mollit enim.</li>
+						<li>Lorem ipsum nulla duis incididunt.</li>
+					</ul>
+				</div>
+				<div class="col-4">
+					<ol>
+						<li>In amet esse nulla.</li>
+						<li>Excepteur dolore.</li>
+						<li>Et cupidatat dolor.</li>
+					</ol>
+				</div>
+				<div class="col-4">
+					<ul class="list--plain">
+						<li>Eu non.</li>
+						<li>Aute quis eu.</li>
+						<li>Mollit ea ut aliquip amet.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
 		
-		<div class="col-12">
-			<h2>Forms</h2>
-			<form action="">
-				<div class="col-4">
-					<label for="">This is a label</label>
-					<input type="text" class="text" placeholder="This is an input...">
-				</div>
-				<div class="col-4">
-					<label for="">This is a label</label>
-					<input type="text" class="text" placeholder="This is an input...">
-				</div>
-				<div class="col-4">
-					<label for="">This is a label</label>
-					<select name="" id="">
-						<option value="" class="select-label">I am an option</option>
-						<option value="">I am an option</option>
-						<option value="">I am an option</option>
-					</select>
-				</div>
-				<div class="col-12">
-					<textarea class="textarea"></textarea>
-				</div>
-				<div class="col-12">
-					<fieldset >
-					    <legend>Fieldset</legend>
-						<div class="col-2">
-						    <input id="radio-1" name="radio" type="radio" checked>
-						    <label for="radio-1" class="radio-label">Checked</label>
-					  	</div>
-						<div class="col-2">
-						    <input id="radio-2" name="radio" type="radio">
-						    <label  for="radio-2" class="radio-label">Unchecked</label>
-						</div>
-						<div class="col-2">
-						    <input id="radio-3" name="radio" type="radio" disabled>
-							<label for="radio-3" class="radio-label">Disabled</label>
-						</div>
-
-						<div class="col-1">
-							<input type="checkbox" name="checkbox">
-							<label for="">Toggle</label>
-						</div>
-					</fieldset>
-				</div>
-				<div class="col-4">
-					<input type="submit" class="button full-width" value="Input Submit" />
-				</div>
-				<div class="col-4">
-					<a href="#" class="button">Text</a>
-				</div>
-			</form>
-		</div>
-
-		<div class="col-12">
-			<section class="wrap">
-			<h3>Responsive Flexbox Table</h3>
-			  <p>I mean, it's still a table. It's just built with flexbox.</p>
-				<ul class="table">
-					<li class="table-head">
-						<span class="title">Title</span>
-						<span class="table-col--2">Description</span>
-						<span class="table-col--3">Language</span>
-						<span class="table-col--4">Category</span>
-						<span class="table-col--5">Updated</span>
-					</li>
-			    	<li>
-						<span class="title">Title 1</span>
-						<span class="table-col--2">Magna dolor cupidatat proident cupidatat sint veniam cillum anim adipisicing deserunt nisi voluptate commodo ea id ad voluptate.</span>
-						<div class="table-col--3">
-							<span class="sub-table-col"><a href="#">ENG</a></span>
-							<span class="sub-table-col"><a href="#">ESP</a></span>
-						</div>
-						<span class="table-col--4">Other</span>
-						<span class="table-col--5">6-17-18</span>
-					</li>
-				  	<li>
-						<span class="title">Title 2</span>
-						<span class="table-col--2">Magna dolor cupidatat proident cupidatat sint veniam cillum anim adipisicing deserunt nisi voluptate commodo ea id ad voluptate.</span>
-						<div class="table-col--3">
-							<span class="sub-table-col"><a href="#">ENG</a></span>
-							<span class="sub-table-col"><a href="#">ESP</a></span>
-						</div>
-						<span class="table-col--4">Other</span>
-						<span class="table-col--5">6-17-18</span>
-					</li>
-				  	<li>
-						<span class="title">Title 3</span>
-						<span class="table-col--2">Magna dolor cupidatat proident cupidatat sint veniam cillum anim adipisicing deserunt nisi voluptate commodo ea id ad voluptate.</span>
-						<div class="table-col--3">
-							<span class="sub-table-col"><a href="#">ENG</a></span>
-							<span class="sub-table-col"><a href="#">ESP</a></span>
-						</div>
-						<span class="table-col--4">Other</span>
-						<span class="table-col--5">6-17-18</span>
-					</li>
-				</ul>	
-			</section>
-		</div>
-
-		<div class="col-1">
-			<div class="example-col">1</div>
-		</div>
-		<div class="col-11">
-			<div class="example-col">11</div>
-		</div>
-		<div class="col-2">
-			<div class="example-col">2</div>
-		</div>
-		<div class="col-10">
-			<div class="example-col">10</div>
-		</div>
-		<div class="col-3">
-			<div class="example-col">3</div>
-		</div>
-		<div class="col-9">
-			<div class="example-col">9</div>
-		</div>
-		<div class="col-4">
-			<div class="example-col">4</div>
-		</div>
-		<div class="col-8">
-			<div class="example-col">8</div>
-		</div>
-		<div class="col-5">
-			<div class="example-col">5</div>
-		</div>
-		<div class="col-7">
-			<div class="example-col">7</div>
-		</div>
-		<div class="col-6">
-			<div class="example-col">6</div>
-		</div>
-		<div class="col-6">
-			<div class="example-col">6</div>
-		</div>
-		<div class="col-7">
-			<div class="example-col">7</div>
-		</div>
-		<div class="col-5">
-			<div class="example-col">5</div>
-		</div>
-		<div class="col-8">
-			<div class="example-col">8</div>
-		</div>
-		<div class="col-4">
-			<div class="example-col">4</div>
-		</div>
-		<div class="col-9">
-			<div class="example-col">9</div>
-		</div>
-		<div class="col-3">
-			<div class="example-col">3</div>
-		</div>
-		<div class="col-10">
-			<div class="example-col">10</div>
-		</div>
-		<div class="col-2">
-			<div class="example-col">2</div>
-		</div>
-		<div class="col-11">
-			<div class="example-col">11</div>
-		</div>
-		<div class="col-1">
-			<div class="example-col">1</div>
-		</div>
-		<div class="col-12">
-			<div class="example-col">12</div>
-		</div>
-	</div>
-
-	<div class="grid-12 example-grid container">
-		<div class="offset-1 col-11">
-			<div class="example-col">offset-1</div>
-		</div>
-		<div class="offset-2 col-10">
-			<div class="example-col">offset-2</div>
-		</div>
-		<div class="offset-3 col-9">
-			<div class="example-col">offset-3</div>
-		</div>
-		<div class="offset-4 col-8">
-			<div class="example-col">offset-4</div>
-		</div>
-		<div class="offset-5 col-7">
-			<div class="example-col">offset-5</div>
-		</div>
-		<div class="offset-6 col-6">
-			<div class="example-col">offset-6</div>
-		</div>
-		<div class="offset-7 col-5">
-			<div class="example-col">offset-7</div>
-		</div>
-		<div class="offset-8 col-4">
-			<div class="example-col">offset-8</div>
-		</div>
-		<div class="offset-9 col-3">
-			<div class="example-col">offset-9</div>
-		</div>
-		<div class="offset-10 col-2">
-			<div class="example-col">offset-10</div>
-		</div>
-		<div class="offset-11 col-1">
-			<div class="example-col">11</div>
-		</div>
-	</div>
-
-	<div class="container">
-		<div class="grid-css example-grid">
-			<div class="col-1-12">
-				<p><code>.grid-css</code> enter <code>.col-#-#</code> to span the columns you need. It's set to end at the column line. Two columns that start and end on the same line will push to the next row. </p>
-				<p>You can enter <code>.row-#-#</code> to position items specifically. Doing this you have to position everything specifically. Things tend to float or jump over other eleents if you don't</p>
-			</div>
-		</div>
-		<div class="grid-css example-grid">
-			<div class="col-1-12">
-				<?php include 'includes/global/header/index.php';?>
-			</div>
-			<div class="col-1-8">
-				<!-- <div class="example-col">Body</div> -->
-				<p>Aute fugiat pariatur qui amet labore incididunt do fugiat occaecat minim dolor in amet id qui est excepteur cupidatat qui in nulla laborum minim excepteur ex sed aute nulla excepteur ad pariatur irure est culpa sed labore aliquip laborum aute exercitation ad ut mollit magna dolore duis veniam dolore anim ut id voluptate officia proident duis esse consectetur in officia dolore in ea anim eu incididunt fugiat ea amet dolore ad est ut cupidatat nostrud incididunt commodo in dolore eu veniam non fugiat in deserunt dolor voluptate exercitation enim anim ut adipisicing ad reprehenderit non exercitation dolor amet excepteur duis exercitation ea pariatur consequat ad est in duis laborum sit ut ullamco pariatur nulla nulla enim elit nisi anim in ut veniam ut esse fugiat consequat velit fugiat cupidatat dolore laborum dolore ullamco adipisicing officia deserunt ea nulla adipisicing in ut fugiat incididunt pariatur ea do anim sint consequat velit consectetur velit labore exercitation cillum laborum ex aliqua aliquip elit dolore labore in amet ex cillum excepteur eiusmod fugiat ad in excepteur aute est qui id dolor eu tempor dolore exercitation aute eiusmod pariatur ut dolore sint in fugiat dolore laboris qui minim dolore id ea ut laborum laboris tempor elit nostrud reprehenderit in est minim aute dolore anim tempor in quis commodo aute cupidatat incididunt eu enim in eu dolor elit dolore do eiusmod tempor pariatur qui esse exercitation reprehenderit.</p>
-				<p>Deserunt exercitation et do ut nulla occaecat amet velit magna dolor labore sit pariatur et tempor ex laborum esse do reprehenderit est fugiat ullamco ut tempor culpa ut aute.</p>
-				<p>Adipisicing aliqua voluptate ad mollit ut minim do ea dolor ut anim tempor dolor eiusmod pariatur in eiusmod dolor veniam veniam minim.</p>
-				<p>Ullamco id et cillum irure mollit dolor voluptate pariatur aute exercitation minim commodo magna incididunt adipisicing aliqua voluptate ut ex anim in et sunt aliquip cillum id elit in ullamco id ex est sed adipisicing ad fugiat sint adipisicing.</p>
-			</div>
-			<div class="col-9-12">
-				<p>Ullamco id et cillum irure mollit dolor voluptate pariatur aute exercitation minim commodo magna incididunt adipisicing aliqua voluptate ut ex anim in et sunt aliquip cillum id elit in ullamco id ex est sed adipisicing ad fugiat sint adipisicing.</p>
-			</div>
-			<div class="col-2-11">
-				<div class="example-col">Content Section</div>
-			</div>
-			<div class="col-1-12">
-				<div class="example-col">Footer</div>
-			</div>
-		</div>
-	</div>
 
 	<?php include 'includes/global/footer/index.php';?>
 	<script src="/js/global.js"></script>
